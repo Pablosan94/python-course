d7746a22968155cee09a0c5ede1dda85e72c1528
Pablo Sanchez <p.sanchez94@gmail.com>
2021-07-08 22:01:49 +0200

    july 8

--- Chapter 2. Flow Control/hilo.py
+++ Chapter 2. Flow Control/hilo.py
@@ -2,20 +2,26 @@ lo = 1
 hi = 1000
 
 print("Please think of a number between {} and {}".format(lo, hi))
-input("Press ENTER to start")
+input("Press ENTER to start\n")
 
-guess = 1
+guesses = 1
 while True:
+  print("Guessing in the range of [{}, {}]\n".format(lo, hi))
   guess = lo + (hi - lo) // 2
-  high_low = input("My guess is {}. Should I guess higher or lower? "
-                   "Enter h or l, or c if my guess was correct"
+  hi_lo = input("My guess is {}. Should I guess higher or lower? "
+                   "Enter h or l, or c if my guess was correct\n"
                    .format(guess)).casefold()
 
-  if high_low == "h":
+  if hi_lo == "h":
     # Guess higher. Low end of the range becomes 1 greater than the guess.
-    print()
-  elif high_low == "l":
+    lo = guess + 1
+  elif hi_lo == "l":
     # Guess lower. High end of the range becomes one less than the guess.
-    print()
-  elif high_low == "c":
-    print("I got it in {} guesses!".format(guess))
+    hi = guess - 1
+  elif hi_lo == "c":
+    print("I got it in {} guesses!".format(guesses))
+    break
+  else:
+    print("Please enter h, l or c")
+
+  guesses += 1
